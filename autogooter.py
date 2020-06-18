73393178cba4912c9934b318f42f46cf4f3cc165
Hiroki1112 <pystdy@yahoo.co.jp>
2020-06-18 13:14:11 +0900

    機能追加

--- autogooter.py
+++ autogooter.py
@@ -1,27 +1,24 @@
-import selenium
-import time,re
+import time,sys,os
 from selenium import webdriver
 import tkinter as tk
-from tkinter import ttk
 from functools import partial
-import pandas as pd
 from random import randint
 from tkinter import messagebox
+import threading
 
 class Application(tk.Frame):
+    
     def __init__(self,master):
         super().__init__(master)
         self.pack()
 
-        self.master.geometry("500x300")
+        self.master.geometry("500x500")
         self.master.title("Note 自動イイネ！")
 
-        with open('./config.csv') as f:
+        with open(self.resource_path('./data/config.csv')) as f:
             self.tmp = f.read()
 
         self.id,self.password = str(self.tmp).split(',')
-        #print(self.tmp)
-
         self.create_widgets()
 
 
@@ -42,7 +39,9 @@ class Application(tk.Frame):
         self.en2.place(x=100,y=80)
         self.en2.insert(tk.END,self.password)
 
-        self.login = tk.Button(self.master,text="LOGIN",width=20,command=self.login)
+        thread1 = threading.Thread(target=self.login)
+
+        self.login = tk.Button(self.master,text="LOGIN",width=20,command=thread1.start)
         self.login.place(x=130, y=120)
 
         self.label3 = tk.Label(text='抽出対象URL : ')
@@ -51,14 +50,26 @@ class Application(tk.Frame):
         self.en3 = tk.Entry(width=40)
         self.en3.place(x=100,y=170)
 
-        self.iinestart = tk.Button(self.master,text="イイネStart!",width=20,command=self.autoiine)
-        self.iinestart.place(x=130, y=200)
-        
+        self.label4 = tk.Label(text='イイネ数 : ')
+        self.label4.place(x=20,y=200)
 
+        self.en4 = tk.Entry(width=10)
+        self.en4.place(x=100,y=200)
+        self.en4.insert(tk.END,30)
+
+        thread2 = threading.Thread(target=self.autoiine)
+
+        self.iinestart = tk.Button(self.master,text="イイネStart!",width=20,command=thread2.start)
+        self.iinestart.place(x=130, y=240)
+
+    def resource_path(self,relative):
+        if hasattr(sys, "_MEIPASS"):
+            return os.path.join(sys._MEIPASS, relative)
+        return os.path.join(relative)
 
     # Event Callback Function
     def login(self):
-        self.driver = webdriver.Chrome(executable_path='driver\\chromedriver.exe')
+        self.driver = webdriver.Chrome(self.resource_path('./driver/chromedriver.exe'))
         url = "https://note.com/login"
         self.driver.get(url)
         time.sleep(4)
@@ -80,11 +91,11 @@ class Application(tk.Frame):
 
         #targetClasses = self.driver.find_element_by_class_name('o-like')
         time.sleep(2)
-        for i in range(1,12):
-            self.driver.execute_script("window.scrollTo(0, {})".format(i*1500))
-            time.sleep(2+randint(0,2))
+        #for i in range(1,int(int(self.en4.get())/10)):
+            #self.driver.execute_script("window.scrollTo(0, {})".format(i*3000))
+            #time.sleep(2+randint(0,2))
 
-        for i in range(50):
+        for i in range(int(self.en4.get())):
             self.driver.find_elements_by_class_name('o-like')[i].click()
             time.sleep(2+randint(0,4))
         
@@ -98,5 +109,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
